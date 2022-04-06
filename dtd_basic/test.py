206d65615459e33981f778d93cba4df785dd9f4c
李嘉琪 <949697127@qq.com>
2022-04-06 22:28:31 +0800

    GP process 01 03-02 progress 02

--- dtd_basic/test.py
+++ dtd_basic/test.py
@@ -2,27 +2,52 @@ import math
 import random
 
 import matplotlib.pyplot as plt
-from matplotlib.image import imread
+import cv2
 import imageio
 from scipy import misc, ndimage
 import numpy as np
+from skimage import img_as_ubyte
+from skimage.exposure import cumulative_distribution
 
 
 def color_space_trans(_image, _para):
-    lx, ly, ch = _image.shape
+    _image = cv2.cvtColor(_image, cv2.COLOR_BGR2RGB)
+    rows, cols = _image.shape[:2]
 
-    # Cropping
     if _para == 0:
-        crop_idx = lx if lx < ly else ly
-        crop = _image[crop_idx // 4: - crop_idx // 4, crop_idx // 4: - crop_idx // 4]
-        return crop
+        # Vignette
+        X_resultant_kernel = cv2.getGaussianKernel(cols, 200)
+        Y_resultant_kernel = cv2.getGaussianKernel(rows, 200)
+        resultant_kernel = Y_resultant_kernel * X_resultant_kernel.T
+        mask = 255 * resultant_kernel / np.linalg.norm(resultant_kernel)
+        output = np.copy(_image)
+        for i in range(3):
+            output[:, :, i] = output[:, :, i] * mask
+        return output
 
-    return _image
+    else:
+        # Random Color Casting
+        chn = random.randint(0, 2)
+        tup = cv2.split(_image)
+        tup[chn] = np.array(tup[chn]) + np.minimum(255 - tup[chn], 100)
+        _image = cv2.merge(tup)
+
+        lookUpTable = np.empty((1, 256), np.uint8)
+        for i in range(256):
+            lookUpTable[0, i] = np.clip(pow(i / 255.0, 1.0) * 255.0, 0, 255)
+
+        return cv2.LUT(_image, lookUpTable)
 
 
 if __name__ == '__main__':
-    f = imread("./data/dtd/images/banded/banded_0019.jpg")
+    f = cv2.imread("./data/dtd/images/banded/banded_0002.jpg")
     file_path = "./data/dtd/images/banded/"
     file_name = "test"
-    f_ct = color_space_trans(f, 2)
-    imageio.imsave(file_path + file_name + '_r.png', f_ct)  # uses the Image module (PIL)
+    f_ct = color_space_trans(f, 0)
+    imageio.imsave(file_path + file_name + '_ct.png', f_ct)
+
+    f_cc = color_space_trans(f, 1)
+    imageio.imsave(file_path + file_name + '_cc.png', f_cc)
+
+    # plt.imshow(f_ct)
+    # plt.show()
